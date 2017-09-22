11f53595277c7009444614b42cde85588e286076
jon_dhc <jonathandaniel.heca@gmail.com>
2017-09-22 17:54:11 -0500

    Added function to get tweets, but it's not fully working. To be solved.

--- bot.py
+++ bot.py
@@ -1,18 +1,43 @@
 import os
-import tweepy
-from secrets import *
+import re
 from time import gmtime, strftime
+
+import tweepy
+
+from secrets import *
 
 # ====== Individual bot configuration ==========================
 bot_username = 'SSNAlerta'
 logfile_name = bot_username + ".log"
 # ==============================================================
 
+def get_tweets(screen_name):
+    # Twitter authentication
+    auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+    auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+    api = tweepy.API(auth)
+    # Initialize a list to hold all the tweets
+    tweet_holder = []
+    new_tweets = api.user_timeline(screen_name=screen_name, count=2000)
+    tweet_holder.extend(new_tweets)
+    for tweet in tweet_holder:
+        searchStr = r"Magnitud ([\d\.]+)"
+        tweetStr = tweet.text
+        magnitude_match = re.search(searchStr, tweetStr)
+        magnitude = float(magnitude_match.group(1))
+        if magnitude >= 4:
+            print "Sismo mayor: " + tweet.text
+        else:
+            print "Menor a 4"
+
+
+
+
 
 def create_tweet():
     """Create the text of the tweet you want to send."""
     # Replace this with your code!
-    text = "Test 5"
+    text = "Test..."
     return text
 
 
@@ -41,5 +66,6 @@ def log(message):
 
 
 if __name__ == "__main__":
+    get_tweets("SismologicoMX")
     tweet_text = create_tweet()
     tweet(tweet_text)
