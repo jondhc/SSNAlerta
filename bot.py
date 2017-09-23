02f89712cac94f5fd132af472a2a42a832ac5770
jon_dhc <jonathandaniel.heca@gmail.com>
2017-09-23 11:20:57 -0500

    Streaming now listening to SismologicoMX events

--- bot.py
+++ bot.py
@@ -12,27 +12,27 @@ bot_username = 'SSNAlerta'
 logfile_name = bot_username + ".log"
 # ==============================================================
 
+# Twitter authentication
+auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+
 class TwitterStreaming(tweepy.StreamListener):
     def __init__(self, api=None):
         self.api = api or API()
 
     def on_status(self, status):
-        print status.text.encode('utf-8') + "\n"
+        if status.user.screen_name == "SismologicoMX":
+            print status.text.encode('utf-8') + "\n"
 
 
 def twitStream():
-    # Twitter authentication
-    auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
-    auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
-    api = tweepy.API(auth)
     stream = tweepy.streaming.Stream(auth, TwitterStreaming())
-    stream.filter(track=['SISMO Magnitud'], languages=['es'])
+    stream.filter(track=['SISMO'], languages=['es'])
+
 
 def get_tweets(screen_name):
-    # Twitter authentication
-    auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
-    auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
-    api = tweepy.API(auth)
     # Initialize a list to hold all the tweets
     tweet_holder = []
     new_tweets = api.user_timeline(screen_name=screen_name, count=200)
@@ -61,11 +61,6 @@ def create_tweet():
 
 def tweet(text):
     """Send out the text as a tweet."""
-    # Twitter authentication
-    auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
-    auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
-    api = tweepy.API(auth)
-
     # Send the tweet and log success or failure
     try:
         api.update_status(text)
