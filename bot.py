5159dc499232569be5bec4a75b0a9b1cfec2eaa4
jon_dhc <jonathandaniel.heca@gmail.com>
2017-09-24 12:08:22 -0500

    Streaming now listening to SismologicoMX events and tweeting seismic events. Testing...

--- bot.py
+++ bot.py
@@ -16,8 +16,11 @@ auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 # Parameters
-topMagnitude = 3
-searchStr = r"Magnitud ([\d\.]+)"
+screen_name = "SismologicoMX"
+minMagnitude = 3
+mwStr = r"Magnitud ([\d\.]+)"
+latStr = r"Lat ([\d\.]+)"
+lngStr = r"Lon ([\d\.]+)"
 
 
 # Twitter Streaming
@@ -26,16 +29,18 @@ class TwitterStreaming(tweepy.StreamListener):
         self.api = api or API()
 
     def on_status(self, status):
-        if status.user.screen_name == "SismologicoMX":
+        if status.user.screen_name == screen_name:
             tweetStr = status.text
-            magnitude_match = re.search(searchStr, tweetStr)
+            magnitude_match = re.search(mwStr, tweetStr)
             if magnitude_match:
                 magnitude = float(magnitude_match.group(1))
-                if magnitude >= topMagnitude:
+                if magnitude >= minMagnitude:
                     api.update_status(tweetStr)
+                    print "Sismo mayor a " + str(minMagnitude) + " grados: " + tweetStr + " Su magnitud: " + str(
+                        magnitude)
 
 
-# Twitter Listening to all tweets with the word SISMO
+# Twitter Listening to all filtered tweets with the word SISMO
 def twitStream():
     stream = tweepy.streaming.Stream(auth, TwitterStreaming())
     stream.filter(track=['SISMO'], languages=['es'])
